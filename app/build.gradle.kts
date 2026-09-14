b8f3ae0d904520baf87d5536e6759f443707951e
dankkj <mailatorresneh@gmail.com>
2026-09-14 19:10:05 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,28 +2,22 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.daniell.controller"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.daniell.controller"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
-
-    buildFeatures {
-        viewBinding = true
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
